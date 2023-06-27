2a38211fd11e09987bbc7637983333bd10a75443
StevenNotovitz <snotovitz@gmail.com>
2023-06-27 09:26:46 -0400

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,19 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-    './storage/framework/views/*.php',
-    './resources/views/**/*.blade.php',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-      },
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  plugins: [require('@tailwindcss/forms')],
+
+    plugins: [require('@tailwindcss/forms')],
 };
